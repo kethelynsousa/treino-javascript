994d462c24575670cf2cd6eae77e089619ee159d
kethelynsousa <kethelynsousa123@gmail.com>
2022-09-06 10:58:56 -0300

    JAVASCRIPT

---
+++ js/exe1_6.js
@@ -0,0 +1,13 @@
+function calcularDistancia() {
+    const gps = {
+
+        disTotal: "1492.56",
+        disPecrorrido: "200",
+
+
+    }
+    document.getElementById("disTotal").innerHTML = disTotal;
+    document.getElementById("disPercorrido").innerHTML = disPecrorrido;
+    document.getElementById("disRestante").innerHTML = disRestante;
+
+}
